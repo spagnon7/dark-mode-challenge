2d1b34e3d289ab7ca000cdd37207535e0bfa6bb2
spagnon7 <spagnon7@gmail.com>
2021-04-15 22:22:32 -0700

    create dark mode state, set themeswitcher button to toggle state and change icon based on state

--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,71 @@
-import React from 'react';
+import React from "react";
 import ThemeSwitcher from "../partials/ThemeSwitcher";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { isDarkMode: false };
+    this.toggleDarkMode = this.toggleDarkMode.bind(this);
+  }
 
-    render() {
-        return (
-            <div className="app">
+  toggleDarkMode() {
+    this.setState({ isDarkMode: !this.state.isDarkMode });
+  }
 
-                <div className="level header">
-                    <div className="title-container">
-                        <div className="morningscore-avatar"></div>
-                        <h1 className="title">Dark Mode Challenge</h1>
-                    </div>
-                    <ThemeSwitcher/>
-                </div>
+  render() {
+    return (
+      <div className="app">
+        <div className="level header">
+          <div className="title-container">
+            <div className="morningscore-avatar"></div>
+            <h1 className="title">Dark Mode Challenge</h1>
+          </div>
+          <ThemeSwitcher
+            isDarkMode={this.state.isDarkMode}
+            toggleDarkMode={this.toggleDarkMode}
+          />
+        </div>
 
+        <div className="columns">
+          <div className="column">
+            <p>
+              Morningscore is the one tool you need to bring in more traffic
+              from Google. Even if you're not an expert, you can focus exactly
+              on the improvements that matter. And with a one-page overview, you
+              don't have to go back and forth between dozens of tools. No need
+              for complex, time-consuming tools. Simplifying SEO is where we
+              specialize.
+            </p>
+          </div>
+          <div className="column">
+            <p>
+              A SEO reporting tool built for leaders. Founded and built in Robo
+              City - Odense, Denmark.
+            </p>
+          </div>
+        </div>
 
-                <div className="columns">
-                    <div className="column">
-                        <p>
-                            Morningscore is the one tool you need to bring in more traffic from Google.
-                            Even if you're not an expert, you can focus exactly on the improvements that matter.
-                            And with a one-page overview, you don't have to go back and forth between dozens of tools.
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Name" />
+          </div>
+        </div>
 
-                            No need for complex, time-consuming tools. Simplifying SEO is where we specialize.
-                        </p>
-                    </div>
-                    <div className="column">
-                        <p>
-                            A SEO reporting tool built for leaders. Founded and built in Robo City -
-                            Odense, Denmark.
-                        </p>
-                    </div>
-                </div>
+        <div className="field">
+          <div className="control">
+            <input className="input" type="text" placeholder="Email" />
+          </div>
+        </div>
 
-                <div className="field">
-                    <div className="control">
-                        <input className="input" type="text" placeholder="Name"/>
-                    </div>
-                </div>
-
-                <div className="field">
-                    <div className="control">
-                        <input className="input" type="text" placeholder="Email"/>
-                    </div>
-                </div>
-
-                <section className="section">
-                    <div className="buttons level-right">
-                        <button className="button">Save</button>
-                        <button type="submit" className="button is-primary">Submit</button>
-                    </div>
-                </section>
-            </div>
-        );
-    }
-
+        <section className="section">
+          <div className="buttons level-right">
+            <button className="button">Save</button>
+            <button type="submit" className="button is-primary">
+              Submit
+            </button>
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoon} from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 export default class ThemeSwitcher extends React.Component {
-
-    render() {
-        return (
-            <button className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
-            </button>
-        );
-    }
-
+  render() {
+    return (
+      <button
+        className="app__dark-mode-btn icon level-right"
+        onClick={this.props.toggleDarkMode}
+      >
+        {this.props.isDarkMode ? (
+          <FontAwesomeIcon icon={faSun} color={"#FFA500"} />
+        ) : (
+          <FontAwesomeIcon icon={faMoon} color={"#4D5B6B"} />
+        )}
+      </button>
+    );
+  }
 }
